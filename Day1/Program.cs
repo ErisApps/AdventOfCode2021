926f65a5fb6c2d0f6c9b592e5847e792c7c6d08e
Eris <erisapps@outlook.be>
2021-12-06 21:43:59 +0100

    ✨ Completed day 1-1

---
+++ Day1/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Hello Day 1-1!");
+
+var depths = File.ReadAllLines(Path.Combine(Environment.CurrentDirectory, "Resources", "day1_1.txt")).Select(int.Parse).ToList();
+var prevDepth = depths.First();
+var increaseCount = 0;
+foreach (var currentDepth in depths)
+{
+	if (currentDepth > prevDepth)
+	{
+		increaseCount++;
+	}
+
+	prevDepth = currentDepth;
+}
+
+Console.WriteLine($"Day 1-1 increase count: {increaseCount}");
