efba3301403224fb7538d6f38043264582c34b67
Eris <erisapps@outlook.be>
2021-12-06 22:00:32 +0100

    ✨ Completed day 1-2

--- Day1/Program.cs
+++ Day1/Program.cs
@@ -1,10 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+Console.WriteLine("Preparing input data");
+var depths = File.ReadAllLines(Path.Combine(Environment.CurrentDirectory, "Resources", "day1.txt")).Select(int.Parse).ToList();
+
 Console.WriteLine("Hello Day 1-1!");
 
-var depths = File.ReadAllLines(Path.Combine(Environment.CurrentDirectory, "Resources", "day1_1.txt")).Select(int.Parse).ToList();
 var prevDepth = depths.First();
 var increaseCount = 0;
+
 foreach (var currentDepth in depths)
 {
 	if (currentDepth > prevDepth)
@@ -16,3 +19,33 @@ foreach (var currentDepth in depths)
 }
 
 Console.WriteLine($"Day 1-1 increase count: {increaseCount}");
+
+
+Console.WriteLine("Hello Day 1-2!");
+
+var slidingWindowQueue = new Queue<int>();
+prevDepth = depths.Take(3).Sum();
+increaseCount = 0;
+
+foreach (var currentDepth in depths)
+{
+	if (slidingWindowQueue.Count >= 3)
+	{
+		slidingWindowQueue.TryDequeue(out _);
+	}
+
+	slidingWindowQueue.Enqueue(currentDepth);
+
+	if (slidingWindowQueue.Count >= 3)
+	{
+		var windowedDepth = slidingWindowQueue.Sum();
+		if (windowedDepth > prevDepth)
+		{
+			increaseCount++;
+		}
+
+		prevDepth = windowedDepth;
+	}
+}
+
+Console.WriteLine($"Day 1-2 increase count: {increaseCount}");
