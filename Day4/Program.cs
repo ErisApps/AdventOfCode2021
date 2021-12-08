1dccd39aa79f113bd3741b6e9ec0dea09ce0fdbd
Eris <erisapps@outlook.be>
2021-12-08 23:17:59 +0100

    ✨ Completed day 4-2

--- Day4/Program.cs
+++ Day4/Program.cs
@@ -7,20 +7,30 @@ var rawBingoData = File
 
 var rawBingoNumberSequence = rawBingoData.First().Split(',');
 
-List<List<string?>> bingoBoards = new();
-List<string?> intermediateBingoBoard = new();
-foreach (var bingoBoardLine in rawBingoData.Skip(2))
-{
-	if (string.IsNullOrWhiteSpace(bingoBoardLine))
-	{
-		bingoBoards.Add(intermediateBingoBoard);
-		intermediateBingoBoard = new(25);
-	}
+List<List<string?>> bingoBoards;
 
-	intermediateBingoBoard.AddRange(bingoBoardLine.Split(" ", StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries));
+void InitializeBingoBoards()
+{
+	bingoBoards = new();
+
+	List<string?> intermediateBingoBoard = new();
+	foreach (var bingoBoardLine in rawBingoData.Skip(2))
+	{
+		if (string.IsNullOrWhiteSpace(bingoBoardLine))
+		{
+			bingoBoards.Add(intermediateBingoBoard);
+			intermediateBingoBoard = new(25);
+		}
+
+		intermediateBingoBoard.AddRange(bingoBoardLine.Split(" ", StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries));
+	}
 }
 
+
 Console.WriteLine("Hello Day 4-1!");
+
+InitializeBingoBoards();
+var bingoNumberSequence = new Queue<string>(rawBingoNumberSequence);
 
 bool ValidateWinCondition(List<string?> bingoBoard)
 {
@@ -36,7 +46,7 @@ bool ValidateWinCondition(List<string?> bingoBoard)
 
 	for (var i = 0; i < bingoBoard.Count / 5; i++)
 	{
-		if (ValidateWinConditionInternal(bingoBoard.SkipWhile((_, index) => (index - i % 5) != 0)))
+		if (ValidateWinConditionInternal(bingoBoard.Where((_, index) => ((index - i) % 5) == 0).ToList()))
 		{
 			return true;
 		}
@@ -47,8 +57,6 @@ bool ValidateWinCondition(List<string?> bingoBoard)
 
 (List<string?> bingoBoard, string winningNumber)? FindWinningBoard()
 {
-	var bingoNumberSequence = new Queue<string>(rawBingoNumberSequence);
-
 	while (bingoNumberSequence.TryDequeue(out var nextBingoNumber))
 	{
 		foreach (var bingoBoard in bingoBoards)
@@ -75,4 +83,49 @@ var findWinningBoard = FindWinningBoard();
 var remainingNumbersScore = findWinningBoard?.bingoBoard.OfType<string>().Select(int.Parse).Sum() ?? 0;
 var winningNumber = int.TryParse(findWinningBoard?.winningNumber, out var winningNumberParsed) ? winningNumberParsed : 0;
 
-Console.WriteLine($"Day 4-1 final winning bingo board score ({remainingNumbersScore} * {winningNumber}) => {remainingNumbersScore * winningNumber}");
+Console.WriteLine($"Day 4-1 winning bingo board final score ({remainingNumbersScore} * {winningNumber}) => {remainingNumbersScore * winningNumber}");
+
+Console.WriteLine("Hello Day 4-2!");
+
+InitializeBingoBoards();
+bingoNumberSequence = new Queue<string>(rawBingoNumberSequence);
+
+List<(List<string?> bingoBoard, string winningNumber)> FindWinningBoards()
+{
+	var winningBoards = new List<(List<string?> bingoBoard, string winningNumber)>();
+
+	while (bingoNumberSequence.TryDequeue(out var nextBingoNumber))
+	{
+		var i = 0;
+		while (i < bingoBoards.Count)
+		{
+			var bingoBoard = bingoBoards[i];
+			for (var j = 0; j < bingoBoard.Count; j++)
+			{
+				if (bingoBoard[j] == nextBingoNumber)
+				{
+					bingoBoard[j] = null;
+				}
+			}
+
+			if (ValidateWinCondition(bingoBoard))
+			{
+				winningBoards.Add((bingoBoard, nextBingoNumber));
+				bingoBoards.RemoveAt(i);
+			}
+			else
+			{
+				i++;
+			}
+		}
+	}
+
+	return winningBoards;
+}
+
+var winningBoards = FindWinningBoards();
+var lastWinningBoard = winningBoards.Last();
+remainingNumbersScore = lastWinningBoard.bingoBoard.OfType<string>().Select(int.Parse).Sum();
+winningNumber = int.TryParse(lastWinningBoard.winningNumber, out winningNumberParsed) ? winningNumberParsed : 0;
+
+Console.WriteLine($"Day 4-1 last winning bingo final board score ({remainingNumbersScore} * {winningNumber}) => {remainingNumbersScore * winningNumber}");
